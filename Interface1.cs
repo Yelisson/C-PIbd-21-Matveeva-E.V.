2c2c48bb54e3dd52cb58f346b3172e80d2d81433
Yelisson <31625378+Yelisson@users.noreply.github.com>
2018-01-03 19:33:59 +0400

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    interface Interface1
+    {
+        void drawAnimal(Graphics g);
+        void moveAnimal(Graphics g);
+        void setPosition(int x, int y);
+        void eatMouse(int countMouse);
+        void eatBirds(int countBirds);
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public abstract class Snake: Interface1
+    {
+        public abstract void drawAnimal(Graphics g);
+        public abstract void moveAnimal(Graphics g);
+
+        protected int startPosX;
+        protected int startPosY;
+
+        protected int countMouse;
+
+        public virtual int MaxCountBirds { protected set; get; }
+
+        protected int countBirds;
+
+        public virtual int MaxCountMouse { protected set; get; }
+        public virtual int MaxSpeed { protected set; get; }
+        public Color ColorBody1 { protected set; get; }
+        public Color ColorBody2 { protected set; get; }
+        public virtual int height { protected set; get; }
+        public void setPosition(int x, int y)
+        {
+            startPosX = x;
+            startPosY = y;
+        }
+
+        public void eatMouse(int count)
+        {
+            if (countMouse + count < MaxCountMouse)
+            {
+                countMouse += count;
+            }
+        }
+        public void eatBirds(int count)
+        {
+            if (countBirds + count < MaxCountBirds)
+            {
+                countBirds += count;
+            }
+        }
+    }
+}
