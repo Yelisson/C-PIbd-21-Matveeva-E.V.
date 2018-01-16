878f4a4fbcd5f151ad692ed659f26f176d1c76f4
Yelisson <31625378+Yelisson@users.noreply.github.com>
2018-01-16 20:06:41 +0400

    Add files via upload

---
+++ Mix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Kitchen_Maffins
+{
+    class Mix
+    {
+        public double mass;
+        //смешиваем ингредиенты
+        public double mixer(Milk m, Sugar s, Flour f, Eggs e)
+        {
+            mass = m.count_milk + s.count_sugar + f.count_flour + e.count_eggs;
+            return mass;
+        }
+    }
+}
